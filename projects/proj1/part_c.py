ed939c009f8f18e795285f0e2edcf701a103b314
Gabriel Wallace <gabe.wallace04@gmail.com>
2020-02-15 12:02:54 -0600

    Add wrapper function for the timeit call and add create_dataset function

--- projects/proj1/part_c.py
+++ projects/proj1/part_c.py
@@ -1,36 +1,42 @@
 """
-PART A: This program compares 
+PART C: This program compares the running time of the recursive and the
+    iterative algorithms to find the nth Fibonacci number
 Author: Gabriel Wallace
 """
 
 import timeit
 
-def fib_rec(n):
-  if n == 0 or n == 1:
-    return n
-  else:
-    return fib_rec(n - 1) + fib_rec(n - 2)
+from part_a import fib_rec
+from part_b import fib_itr
 
-def fib_itr(n):
-  f_1 = 0
-  f_2 = 1
-  if n == 0:
-    return f_1
-  elif n == 1:
-    return f_2
-  else:
-    for i in range(2, n + 1):
-      f_3 = f_1 + f_2
-      f_1 = f_2
-      f_2 = f_3
-    return f_2
-  
+def wrapper(func, *args, **kwargs):
+    """
+    This is a wrapper to make the timeit call more readable
+    """
+    def wrapped():
+        return func(*args, **kwargs)
+    return wrapped
+
+def create_dataset(some_func, rng: int, *args: str) -> list:
+    """
+    This functions creates a dataset of the times for some_func to run
+    """
+    header = [*args]
+    lol = [header]
+    for i in range(0, rng + 1):
+        wrapped = wrapper(some_func, i)
+        time = timeit.timeit(wrapped)
+        temp = [i, time]
+        lol.append(temp)
+    return lol
 
 if __name__ == "__main__":
-  print("n,time_itr,time_rec")
-  for i in range(0, 21):
-    time_rec = timeit.timeit("fib_rec(i)", "from __main__ import fib_rec, i")
-    time_itr = timeit.timeit("fib_itr(i)", "from __main__ import fib_itr, i")
-    print("{},{},{}".format(i, time_itr, time_rec))
-  
-  
+    """
+    Create the two datasets for the recursive and iterative algs
+    and then output to recursive.csv and iterative.csv
+    """
+    #Only use 20 for recursive
+    rec = create_dataset(fib_rec, 20, "n", "time")
+    #Use 100 for iterative
+    itr = create_dataset(fib_itr, 100, "n", "time")
+
