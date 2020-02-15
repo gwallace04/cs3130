4363aced814df030c569b01a0e16fe4e1de84ea9
Gabriel Wallace <gabe.wallace04@gmail.com>
2020-02-15 12:49:37 -0600

    Add comments and main function

--- projects/proj1/part_d.py
+++ projects/proj1/part_d.py
@@ -1,29 +1,38 @@
+"""
+PART D: This program outputs the largest Fiboncci number with n digits using 
+        an algorithm that adds the digits in an array
+Author: Gabriel Wallace
+"""
 
 def add_digits(a: list, b: list) -> list: 
-  carry = 0
-  max_size = max(len(a), len(b))
-  c = [None] * max_size
-  a.extend([0] * (len(b) - len(a)))
-  b.extend([0] * (len(a) - len(b)))
-  for i in range(0, max_size):
-    c[i] = (a[i] + b[i] + carry) % 10
-    carry = (a[i] + b[i] + carry) // 10
-  if carry != 0:
-    c.append(carry)
-  return c
+    carry = 0
+    max_size = max(len(a), len(b))
+    c = [None] * max_size
+    a.extend([0] * (len(b) - len(a)))
+    b.extend([0] * (len(a) - len(b)))
+    for i in range(0, max_size):
+        c[i] = (a[i] + b[i] + carry) % 10
+        carry = (a[i] + b[i] + carry) // 10
+    if carry != 0:
+        c.append(carry)
+    return c
 
 def fib_with_n_digits(n: int) -> list:
-  a = [0]
-  b = [1]
-  while (len(b) < n + 1):
-    c = add_digits(a, b)
-    a = b
-    b = c
-  return a
+    a = [0]
+    b = [1]
+    while (len(b) < n + 1):
+        c = add_digits(a, b)
+        a = b
+        b = c
+    return a
 
 
 if __name__ == "__main__":
-  fib_list = fib_with_n_digits(1000)
-  fib_string = ''.join(map(str, fib_list))
-  fib_string = fib_string[::-1]
-  print(fib_string)
+    print("Find the largest Fibonacci number with n digits")
+    n = int(input("Enter an integer (<= 100): "))
+    while n > 100:
+        n = int(input("Enter an integer (<= 100): "))
+    fib_list = fib_with_n_digits(n)
+    fib_string = ''.join(map(str, fib_list))
+    fib_string = fib_string[::-1]
+    print("The largest Fibonnaci number with {} digits is {}".format(n, fib_string))
