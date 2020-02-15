dfabaaf00c3ede2fbf84815572c81d065d8d2c12
Gabriel Wallace <gabe.wallace04@gmail.com>
2020-02-15 17:12:50 -0600

    Change file names to current data sets

--- projects/proj1/plot_results.R
+++ projects/proj1/plot_results.R
@@ -1,15 +1,13 @@
 library(tidyverse)
 
-results <- read_csv("Extra/results.csv")
-results <- rowid_to_column(results, "number")
-
-iterative <- read_csv("Extra/iterative.csv")
+iterative <- read_csv("iterative.csv")
+recursive <- read_csv("recursive.csv")
 
 # ggplot(data.frame(num = 1:length(results), rec = results$recursive), aes(x = num, y = rec)) + geom_line()
 
-results %>% 
-  ggplot(aes(x = number)) + geom_line(aes(y = recursive)) + geom_line(aes(y = iterative))
-
 iterative %>%  
-  ggplot(aes(x = n, y = time)) + geom_line() + geom_smooth()
+  ggplot(aes(x = n, y = time)) + geom_point() + geom_smooth(se = FALSE)
+
+recursive %>% 
+  ggplot(aes(x = n, y = time)) + geom_point() + geom_smooth(se = FALSE)
 
