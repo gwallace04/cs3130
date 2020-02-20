01355de753a03e23c98b4a2b01b9060a7bc79e6a
Gabriel Wallace <gabe.wallace04@gmail.com>
2020-02-19 20:40:56 -0600

    Fix formatting

--- projects/proj1/plot_results.R
+++ projects/proj1/plot_results.R
@@ -6,8 +6,12 @@ recursive <- read_csv("recursive.csv")
 # ggplot(data.frame(num = 1:length(results), rec = results$recursive), aes(x = num, y = rec)) + geom_line()
 
 iterative %>%  
-  ggplot(aes(x = n, y = time)) + geom_point() + geom_smooth(se = FALSE)
+  ggplot(aes(x = n, y = time)) 
+  + geom_point() 
+  + geom_smooth(se = FALSE)
 
 recursive %>% 
-  ggplot(aes(x = n, y = time)) + geom_point() + geom_smooth(se = FALSE)
+  ggplot(aes(x = n, y = time)) 
+  + geom_point() 
+  + geom_smooth(se = FALSE)
 
