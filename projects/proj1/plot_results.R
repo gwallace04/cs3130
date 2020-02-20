5104c924a799e287ae4f5bde49c17d60c11a9991
Gabriel Wallace <gabe.wallace04@gmail.com>
2020-02-19 20:45:53 -0600

    Add titles and labels to graphs

--- projects/proj1/plot_results.R
+++ projects/proj1/plot_results.R
@@ -6,12 +6,16 @@ recursive <- read_csv("recursive.csv")
 # ggplot(data.frame(num = 1:length(results), rec = results$recursive), aes(x = num, y = rec)) + geom_line()
 
 iterative %>%  
-  ggplot(aes(x = n, y = time)) 
-  + geom_point() 
-  + geom_smooth(se = FALSE)
+  ggplot(aes(x = n, y = time)) +
+  ggtitle("Iterative algorithm") +
+  geom_point() +
+  geom_smooth(se = FALSE) + 
+  ylab("Time (sec)") 
+
 
 recursive %>% 
-  ggplot(aes(x = n, y = time)) 
-  + geom_point() 
-  + geom_smooth(se = FALSE)
+  ggplot(aes(x = n, y = time)) +
+  geom_point() +  
+  geom_smooth(se = FALSE) +
+  ylab("Time (sec)")
 
