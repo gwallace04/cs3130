c59ec4dc390258f7ff9d3a883fb57fcf7add30d8
Gabriel Wallace <gabe.wallace04@gmail.com>
2020-02-28 10:34:06 -0600

    Add more tests

--- projects/proj2/proj2.py
+++ projects/proj2/proj2.py
@@ -7,7 +7,7 @@ def bubble(A: list) -> list:
     output: a list sorted in nondecreasing order
     """
     n = len(A)
-    for i in range(n - 2):
+    for i in range(n - 1):
         for j in range(n - 1 - i):
             if A[j + 1] < A[j]:
                 A[j], A[j + 1] = A[j + 1], A[j]
--- projects/proj2/test_proj2.py
+++ projects/proj2/test_proj2.py
@@ -18,8 +18,24 @@ class TestSortingAlgs(unittest.TestCase):
         input_list = []
         self._test_all_funcs(input_list)
 
-    def test_with_10_integers(self):
+    def test_with_random_integers(self):
         input_list = [56, 18, 51, 6, 93, 6, 37, 57, 87, 67]
+        self._test_all_funcs(input_list)
+
+    def test_with_sorted(self):
+        input_list = [i for i in range(10)]
+        self._test_all_funcs(input_list)
+
+    def test_with_almost_sorted(self):
+        input_list = [46, 1, 2, 55, 4, 5, 87, 7, 8, 64] 
+        self._test_all_funcs(input_list)
+
+    def test_with_reverse_sorted(self):
+        input_list = [i for i in range(10, 0, -1)] 
+        self._test_all_funcs(input_list)
+
+    def test_with_all_same(self):
+        input_list = [1 for i in range(10)]
         self._test_all_funcs(input_list)
 
 if __name__ == "__main__":
