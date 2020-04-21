c769ec597a8899a3d859b9ce51645878c4ddad20
Gabriel Wallace <gabe.wallace04@gmail.com>
2020-04-20 22:03:37 -0500

    Add to main

--- projects/proj3/part_b.py
+++ projects/proj3/part_b.py
@@ -8,7 +8,7 @@ import random
 
 from bst import Tree, Node
 
-def get_avg_height(N: int, t: int) -> float:
+def get_avg_height(N: int, t: int, print_statements=False) -> float:
     height_sum = 0
     for i in range(t):
         rand = [random.randrange(1, 501) for x in range(N)]
@@ -17,11 +17,18 @@ def get_avg_height(N: int, t: int) -> float:
             tree.insert(x)
 
         height_sum += tree.height()
-        print("The height of the tree is", tree.height())
+        if print_statements:
+            print("The height of the tree is", tree.height())
     avg = height_sum / t 
-    print("The average height is", avg)
+    if print_statements:
+        print("The average height is", avg)
     return avg
         
 
 if __name__ == "__main__":
-    get_avg_height(100, 5)
+    print(get_avg_height(100, 5, print_statements=True))
+
+    for N in [100, 500, 1000]:
+        for t in [5, 10, 15]:
+            print("{}, {}, {}".format(
+                N, t, get_avg_height(N, t)))
